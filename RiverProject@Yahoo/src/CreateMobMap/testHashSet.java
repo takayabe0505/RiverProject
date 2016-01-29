e357fb9501d261094648a8a026d4c878282ef156
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2016-01-29 18:00:58 +0900

    final commit

---
+++ RiverProject@Yahoo/src/CreateMobMap/testHashSet.java
@@ -0,0 +1,25 @@
+package CreateMobMap;
+
+import java.util.HashSet;
+
+public class testHashSet {
+
+	public static void main(String args[]){
+		
+		HashSet<String> victimID = new HashSet<String>();
+		method1(victimID);
+		method2(victimID);
+		
+		System.out.println(victimID);
+	}
+	
+	public static void method1(HashSet<String> victimID){
+		victimID.add("007");
+		victimID.add("James Bond");
+	}
+	
+	public static void method2(HashSet<String> victimID){
+		victimID.add("James Bond");
+		victimID.add("James Bond 007");
+	}
+}
