7313a08b8ec9349bf57ec80a6dca6c2d02940fc8
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2016-02-04 16:41:12 +0900

    sadasdsa

--- RiverProject@Yahoo/src/AnalysisofExpData/MovementLength.java
+++ RiverProject@Yahoo/src/AnalysisofExpData/MovementLength.java
@@ -24,12 +24,14 @@ public class MovementLength {
 
 		File IDFile = new File(basicpath+"IDswithhomesinArea.csv");
 		HashSet<String> IDs = getIDs(IDFile);
+		System.out.println("got IDs:" + IDs.size());
 
 		File res = new File(basicpath+"flood_length.csv");
-		BufferedWriter bw = new BufferedWriter(new FileWriter(res));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(res,true));
 		
 		File in = new File(basicpath+"logsofIDwithhomesinArea20150910_from07.csv");
 		HashMap<String, HashMap<String, HashMap<Integer,LonLat>>> alldata = getallData(in,IDs);
+		System.out.println("size of map:" +alldata.size());
 		for(String date : alldata.keySet()){
 			for(String id : alldata.get(date).keySet()){
 				Double distance = calculateLength(alldata.get(date).get(id));
